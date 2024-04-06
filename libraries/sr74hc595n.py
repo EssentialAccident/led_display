f22156c8b2f8421773003dec8ae0a714dd7c7160
essential accident <essentialaccident@gmail.com>
2024-04-06 11:16:08 -0400

    Moving files and adding brightness to display

--- libraries/sr74hc595n.py
+++ libraries/sr74hc595n.py
@@ -4,7 +4,7 @@
 # 2024                                          #      
 #################################################
 
-from machine import Pin
+from machine import Pin, PWM
 from time import sleep
 
 SLEEP_TIME = 0
@@ -20,13 +20,14 @@ class Sr74hc595n:
         #                   For the normal operation of the register, 
         #                   it has to be pulled high
         # self.out_enable:  It disables the outputs of the shift register
-        #                   when pulled low
+        #                   when pulled low.
+        #                   It will be configured as a PWM pin.
         
+        # Creating pins
         self.data = Pin(data, Pin.OUT)
         self.clock = Pin(clock, Pin.OUT)
         self.latch = Pin(latch, Pin.OUT)
         self.clear_register = Pin(clear, Pin.OUT)
-        self.out_enable = Pin(out_enable, Pin.OUT)
         self.num_registers = num_registers
 
         # Setting the initial states of the Shift Register pins
@@ -34,7 +35,13 @@ class Sr74hc595n:
         self.clock.off()
         self.latch.off()
         self.clear_register.on()
-        self.out_enable.off()
+        
+        # The output enable pin will be setup as PWM
+        self.pwm_freq = 5000
+        pin_out_enable = Pin(out_enable, Pin.OUT)
+        self.pwm_output = PWM(pin_out_enable, freq=self.pwm_freq, invert=True)       
+        # The display will be always initialized full brightness
+        self.brightness(100)
 
         # Set up the register
         self.clear_register()
@@ -46,14 +53,11 @@ class Sr74hc595n:
         self.clear_register.on()
         self.__pulse_latch()
 
-    def outputs_enable(self, value: bool) -> None:
-        if value:
-            self.out_enable.off()
-        if not value:
-            self.out_enable.on()
-
-    def outputs_enable_toggle(self) -> None:
-        self.out_enable.toggle()
+    def brightness(self, percent: int) -> None:
+        # The duty cycle of the PWM is a range from 0 to 65535
+        # Converting the percent enter to a 0 to 65535 range
+        duty_cycle = int((percent / 100) * 65535)
+        self.pwm_output.duty_u16(duty_cycle)
 
     def __pulse_clock(self) -> None:
         self.clock.on()
@@ -70,4 +74,8 @@ class Sr74hc595n:
             self.data.value(bit)
             self.__pulse_clock()
         self.__pulse_latch()
+    
+    def deinit(self) -> None:
+        self.clear()
+        self.pwm_output.deinit()
 
--- main.py
+++ main.py
@@ -13,13 +13,17 @@ sr = Sr74hc595n(data=11, clock=12, latch=13, clear=14, out_enable=15)
 
 def main():
     sr.write(0b10101010)
-    while True:
-        sr.outputs_enable_toggle()
+    for i in range(0, 100, 10):
+        sr.brightness(i)
         sleep(0.5)
+    for i in range(100, 0, -10):
+        sr.brightness(i)
+        sleep(0.5)
+    sr.brightness(0)
 
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        sr.clear()
+        sr.deinit()
